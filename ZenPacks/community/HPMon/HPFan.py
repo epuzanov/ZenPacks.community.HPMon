9e956e1870968e46a8cb74c1d45be876e404ca90
Egor Puzanov <epuzanov@gmx.de>
2011-01-26 20:36:04 +0100

    fix import from HPComponent

--- ZenPacks/community/HPMon/HPFan.py
+++ ZenPacks/community/HPMon/HPFan.py
@@ -12,12 +12,12 @@ __doc__="""HPFan
 
 HPFan is an abstraction of a fan or probe.
 
-$Id: HPFan.py,v 1.2 2011/01/04 23:11:37 egor Exp $"""
+$Id: HPFan.py,v 1.3 2011/01/26 20:35:15 egor Exp $"""
 
-__version__ = "$Revision: 1.2 $"[11:-2]
+__version__ = "$Revision: 1.3 $"[11:-2]
 
 from Products.ZenModel.Fan import Fan
-from HPComponent import HPComponent
+from HPComponent import *
 
 class HPFan(Fan, HPComponent):
     """Fan object"""
